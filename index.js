e613c14a965388aa996ee8391ef571c11da5b793
Tony Kharioki <antolisah@gmail.com>
2021-05-15 18:30:19 +0300

    verify token and make secret permanent

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const { Config } = require('node-json-db/dist/lib/JsonDBConfig');
 
 const app = express();
 
+// middleware
+app.use(express.json())
+
 // initialize db with node-json-db
 const db = new JsonDB(new Config('myDatabase', true, false, '/'))
 
@@ -26,6 +29,35 @@ app.post('/api/register', (req, res) => {
         res.status(500).json({ message: 'Error generating the secret' })
     }
 
+})
+
+// Verify token and make secret perm
+app.post('/api/verify', (req, res) => {
+    const { token, userId } = req.body
+
+    try {
+        const path = `/user/${userId}`
+        const user = db.getData(path)
+
+        const { base32: secret } = user.temp_secret
+
+        const verified = speakeasy.totp.verify({
+            secret,
+            encoding: 'base32',
+            token
+        });
+
+        if(verified) {
+            db.push(path, { id: userId, secret: user.temp_secret })
+            res.json({ verified: true })
+        } else {
+            res.json({ verified: false })
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: 'Error finding user' })
+    }
 })
 
 const PORT = process.env.PORT || 5000;
