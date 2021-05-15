6895fd67f72aad2f76369826d861a7cda50ff87e
Tony Kharioki <antolisah@gmail.com>
2021-05-15 18:44:39 +0300

    validate token

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.post('/api/register', (req, res) => {
 
 })
 
-// Verify token and make secret perm
+// Verify token and make secret permanent
 app.post('/api/verify', (req, res) => {
     const { token, userId } = req.body
 
@@ -52,6 +52,35 @@ app.post('/api/verify', (req, res) => {
             res.json({ verified: true })
         } else {
             res.json({ verified: false })
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: 'Error finding user' })
+    }
+})
+
+// Validate a token
+app.post('/api/validate', (req, res) => {
+    const { token, userId } = req.body
+
+    try {
+        const path = `/user/${userId}`
+        const user = db.getData(path)
+
+        const { base32: secret } = user.secret
+
+        const tokenValidates = speakeasy.totp.verify({
+            secret,
+            encoding: 'base32',
+            token,
+            window: 1
+        });
+
+        if(tokenValidates) {
+            res.json({ validated: true })
+        } else {
+            res.json({ validated: false })
         }
 
     } catch (error) {
